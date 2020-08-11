0138240a56593edb42546726b7b4f590762a5f5f
thellamalover4real <bigpapamack@gmail.com>
2020-08-11 17:58:49 -0500

    updated models to reflect intent of client

--- statWebApp/encounter/models.py
+++ statWebApp/encounter/models.py
@@ -2,24 +2,35 @@ from django.db import models
 
 
 class AgeRange(models.Model):
-	primary_key = models.IntegerField(primary_key=true)
+	primary_key = models.IntegerField(primary_key = true)
 	lower_age_range = models.IntegerField()
 	higher_age_range = models.IntegerField()
 	range_text = models.CharField(max_length=50)
-
-
-class Patient(models.Model):
-	age = models.ForeignKey(AgeRange, on_delete = models.CASCADE)
-	latitude = models.IntegerField()
-	longitude = models.IntegerField()
+	
 
 class Disease(models.Model):
-	primary_key = models.IntegerField(primary_key=true)
+	primary_key = models.IntegerField(primary_key = true)
 	disease_name = models.CharField(max_length = 50)
 
+class PatientDiseaseGroup(models.Model):
+	primary_key = modles.IntegerField(primary_key = true)
+
+class DiagnosisDiseaseGroupEntry(models.Model):
+	primary_key = models.IntegerField(primary_key)
+	disease_key = models.ForeignKey(Disease, on_delete = models.CASCADE)
+	patient_disease_group_key = models.ForeignKey(PatientDiseaseGroup, on_delete = models.CASCADE)
+
+
 class Encounter(models.Model):
-	patient = models.ForeignKey(Patient, on_delete = models.CASCADE)
-	diagnosis = models.ForeignKey(disease, on_delete = models.CASCADE)
+	age = models.ForeignKey(AgeRange, on_delete = models.CASCADE)
+	gender = models.CharField(max_length = 10)
+	follow_up_status = models.CharField(max_length = 12)
+	inpatient_outpatient_status = models.CharField(max_length = 12)
+	latitude = models.IntegerField()
+	longitude = models.IntegerField()
+	diagnosis = models.ForeignKey(DiagnosisDiseaseGroup, on_delete = models.CASCADE)
 	date = models.DateField()
+	notes = models.CharField(max_length = 1000)
+	
 
 # Create your models here.
