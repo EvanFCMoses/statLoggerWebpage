09b19ecab795e2d40bb85c6dca4e68ae81fcf452
evan moses <evanfcmoses@gmail.com>
2021-02-21 17:53:40 -0500

    changing to use templates, stubbed

--- statWebApp/encounter/views.py
+++ statWebApp/encounter/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+drom django.views import View
 
-def index(request):
-	return HttpResponse("Location of encounter view.")
-# Create your views here.
+class Index(View):
+	template = 'index.html'
+
+	def get(self, request):
+		return render(request, self.template)
