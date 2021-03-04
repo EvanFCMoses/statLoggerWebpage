406a238d208d01073b3a3df61712374807432f59
Evan Moses <evanfcmoses@gmail.com>
2021-03-04 01:06:14 +0000

    fixed static issues

--- statWebApp/statWebApp/settings.py
+++ statWebApp/statWebApp/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+print("base dir path" + BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -123,7 +124,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_URL = 'static/'
+STATIC_ROOT = '/var/www/impromptumachinactions.us/encounter/'
+
+STATICFILES_DIRS = (
+    '/home/evanm/git/statLoggerWebpage/statWebApp/assets',
+)
