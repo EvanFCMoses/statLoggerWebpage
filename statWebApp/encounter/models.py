c00e2e69dee003117a86054db57dc5b9b4172ed9
evan moses <evanfcmoses@gmail.com>
2021-05-31 15:33:34 -0400

    fixing models primary keys

--- statWebApp/encounter/models.py
+++ statWebApp/encounter/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 
 class AgeRange(models.Model):
-	primary_key = models.IntegerField(primary_key = true)
+	primary_key = models.IntegerField(primary_key = True)
 	lower_age_range = models.IntegerField()
 	higher_age_range = models.IntegerField()
 	range_text = models.CharField(max_length=50)
 	
 
 class Disease(models.Model):
-	primary_key = models.IntegerField(primary_key = true)
+	primary_key = models.IntegerField(primary_key = True)
 	disease_name = models.CharField(max_length = 50)
 
 class PatientDiseaseGroup(models.Model):
-	primary_key = modles.IntegerField(primary_key = true)
+	primary_key = models.IntegerField(primary_key = True)
 
 class DiagnosisDiseaseGroupEntry(models.Model):
-	primary_key = models.IntegerField(primary_key)
+	primary_key = models.IntegerField(primary_key = True)
 	disease_key = models.ForeignKey(Disease, on_delete = models.CASCADE)
 	patient_disease_group_key = models.ForeignKey(PatientDiseaseGroup, on_delete = models.CASCADE)
 
