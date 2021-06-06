f11251e2518a9d5b5d871264bc3739b7eb1cd61a
evan moses <evanfcmoses@gmail.com>
2021-06-06 12:39:09 -0400

    logging to individual file

--- statWebApp/statWebApp/settings.py
+++ statWebApp/statWebApp/settings.py
@@ -19,13 +19,18 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '/var/log/django/debug.log',
         },
     },
-    'root': {
-        'handlers': ['console'],
-        'level': 'DEBUG',
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
     },
 }
 
