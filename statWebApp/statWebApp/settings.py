773fe00bad319c4f24870462940366c470f46920
evan moses <evanfcmoses@gmail.com>
2021-06-06 12:01:36 -0400

    adding rudimentary logging

--- statWebApp/statWebApp/settings.py
+++ statWebApp/statWebApp/settings.py
@@ -15,7 +15,19 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-print("base dir path" + BASE_DIR)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
