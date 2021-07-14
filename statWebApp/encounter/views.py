e1ebe0b53ab49079abc69bbde3116612d9b7ac23
evan moses <evanfcmoses@gmail.com>
2021-07-13 22:59:38 -0400

    pushing to the same url

--- statWebApp/encounter/views.py
+++ statWebApp/encounter/views.py
@@ -15,14 +15,24 @@ class DataEntry(View):
 	def get(self, request):
 		return render(request, self.template)
 
+	def post(self, request):
+		logger = logging.getLogger('django')
+		logger.debug('here is the request object fixify')
+		logger.debug('keys:')
+		message = request.POST
+		for key, value in message.items():
+			logger.debug(key)
+			logger.debug(value)
+		return render(request, 'dataEntry.html')
 
-def process(request):
-	logger = logging.getLogger('django')
-	logger.debug('here is the request object fixify')
-	logger.debug('keys:')
-	message = request.POST
-	for key, value in message.items():
-		logger.debug(key)
-		logger.debug(value)
-	return render(request, 'dataEntry.html')
+
+# def process(request):
+# 	logger = logging.getLogger('django')
+# 	logger.debug('here is the request object fixify')
+# 	logger.debug('keys:')
+# 	message = request.POST
+# 	for key, value in message.items():
+# 		logger.debug(key)
+# 		logger.debug(value)
+# 	return render(request, 'dataEntry.html')
 
