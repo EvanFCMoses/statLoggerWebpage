eb1f414790536d4cff4a559557d1462904bdbd01
evan moses <evanfcmoses@gmail.com>
2021-07-15 23:17:29 -0400

    saving data first pass

--- statWebApp/encounter/views.py
+++ statWebApp/encounter/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.template import loader
-from .models import Disease
+from .models import Disease, Encounter, DiagnosisDiseaseGroupEntry
 import logging
 
 class Index(View):
@@ -18,12 +18,13 @@ def dataEntry(request):
 		logger.debug('here is the request object fixify')
 		logger.debug('keys:')
 		message = request.POST
+		processAndSaveNewEntry(message.items)
 		for key, value in message.items():
 			logger.debug(key)
 			logger.debug(value)
 
 		return returnDataEntry(request)
-
+	
 	elif request.method == 'GET':
 		return returnDataEntry(request)
 
@@ -34,3 +35,41 @@ def returnDataEntry(request):
 		'all_diseases': all_diseases,
 	}
 	return HttpResponse(template.render(context, request))
+
+class savePost(self):
+	def updateGender(encounter, gender):
+		encounter.gender = gender
+	def updatePatientAge(encounter, patientAge):
+		encounter.patientAge = patientAge
+	def updateClinicLocation(encounter, clinicLocation):
+		encounter.clinicLocation = clinicLocation
+	def updatePriorPatient(encounter, priorPatient):
+		encounter.priorPatient = priorPatient
+	def updateRolePlayed(encounter, rolePlayed):
+		encounter.rolePlayed = rolePlayed
+	def updateNotes(encounter, notes):
+		encounter.notes = notes
+	def addDiagnosis(encounter, diagnosis):
+		newDiagnosis, created = Disease.objects.get_or_create(disease_name=diagnosis, stub_indicator=True) #FIXIFY stubRemoval
+		newDiagnosis.save()
+		diagnosisLookupTable = DiagnosisDiseaseGroupEntry(disease_key=newDiagnosis, encounter_key=encounter)
+		diagnosisLookupTable.save()
+
+	inputMap = {
+		"gender":updateGender,
+		"patientAge":updatePatientAge,
+		"clinicLocation":updateClinicLocation,
+		"priorPatient":updatePriorPatient,
+		"rolePlayed":updateRolePlayed,
+		"notes":updateNotes,
+	}	
+
+	def processAndSaveNewEntry(messageItems):
+		encounter = Encounter()
+		for key, value in messageItems:
+			if key in inputMap:
+				func = inputMap.get(key, "nothing")
+				func(encounter, value)
+			else:
+				addDiagnosis(encounter, value)
+
