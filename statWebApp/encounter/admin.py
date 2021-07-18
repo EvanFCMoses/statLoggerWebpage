952489965a1a9ef637fb86562a2e68de750b04a6
Evan Moses <evanfcmoses@gmail.com>
2021-07-18 17:44:38 -0400

    adding models to admin

--- statWebApp/encounter/admin.py
+++ statWebApp/encounter/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Encounter, Disease, DiagnosisDiseaseGroupEntry
+
+admin.site.register(Encounter)
+admin.site.register(Disease)
+admin.site.register(DiagnosisDiseaseGroupEntry)
+
 # Register your models here.
