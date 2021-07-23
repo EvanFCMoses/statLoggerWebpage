34b49e9b1d0a5ec9815589aa669ad00d2a8f6bc5
evan moses <evanfcmoses@gmail.com>
2021-07-22 21:59:45 -0400

    adding default model str

--- statWebApp/encounter/models.py
+++ statWebApp/encounter/models.py
@@ -5,6 +5,9 @@ class Disease(models.Model):
 	disease_name = models.CharField(max_length = 50)
 	stub_indicator = models.BooleanField(default = False)
 
+	def __str__(self):
+		return self.disease_name
+
 
 class Encounter(models.Model):
 	age = models.IntegerField(null=True)
@@ -18,7 +21,13 @@ class Encounter(models.Model):
 	notes = models.CharField(max_length = 1000, null=True)
 	test_indicator = models.BooleanField(default = False)
 
+	def __str__(self):
+		return str(self.date) + ' ' + self.clinicLocation + ' ' + self.notes
+
 
 class DiagnosisDiseaseGroupEntry(models.Model):
 	disease_key = models.ForeignKey(Disease, on_delete = models.CASCADE)
 	encounter_key = models.ForeignKey(Encounter, on_delete = models.CASCADE)
+
+	def __str__(self):
+		return 'encounter #' + str(self.encounter_key)
