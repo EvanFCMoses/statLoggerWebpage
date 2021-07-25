27fa0b16925e3f68d90606242a785ff462d673d2
evan moses <evanfcmoses@gmail.com>
2021-07-24 22:34:12 -0400

    including auth

--- statWebApp/encounter/urls.py
+++ statWebApp/encounter/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, auth
 from encounter import views
 from . import views
 
 urlpatterns = [
         path('admin/', admin.site.urls),
-	path('', views.dataEntry, name='dataEntry'),
+	path('', views.dataEntry, name='dataEntry', include('django.contrib.auth.urls')),
 ]
